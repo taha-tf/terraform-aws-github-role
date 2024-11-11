7756ea3e002ea498cf8b0721827ca237a0e9985a
taha-tf <144951955+taha-tf@users.noreply.github.com>
2024-11-11 15:24:49 -0800

    add optional role name (#4)
    * Configure CD for terraform-aws-github-role (#3)
    
    Co-authored-by: Oleksandr Kuzminskyi <aleks@infrahouse.com>
    
    * Update variables.tf
    
    Signed-off-by: Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
    Co-authored-by: Oleksandr Kuzminskyi <aleks@infrahouse.com>
    Co-authored-by: Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "role_name" {
+  description = "Name of the role. If left unset, the role name will be `ih-tf-${var.repo_name}-github`."
+  type        = string
+  default     = null
+}
+
 variable "gh_org_name" {
   description = "GitHub organization name."
   type        = string
