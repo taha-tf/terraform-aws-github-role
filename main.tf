d2b10b4fc82e3a01cc3ad3016a7eb998eb1ae0a0
Taha Junaid <taha@tinyfish.io>
2024-11-11 15:12:42 -0800

    Configure CD for terraform-aws-github-role (#3)
    Co-authored-by: Oleksandr Kuzminskyi <aleks@infrahouse.com>

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role" "github" {
-  name               = "ih-tf-${var.repo_name}-github"
-  description        = "Role for a GitHub Actions runner in repo ${var.repo_name}"
+  name               = var.role_name == null ? "ih-tf-${var.repo_name}-github" : var.role_name
+  description        = "Role for a GitHub Actions runner in repo ${var.repo_name} with suffix ${var.name_suffix}"
   assume_role_policy = data.aws_iam_policy_document.github-trust.json
   tags               = local.tags
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "role_name" {
+  description = "Name of the role."
+  type        = string
+  default     = null
+}
+
 variable "gh_org_name" {
   description = "GitHub organization name."
   type        = string
@@ -5,4 +11,9 @@ variable "gh_org_name" {
 
 variable "repo_name" {
   description = "Repository name in GitHub. Without the organization part."
+}
+
+variable "name_suffix" {
+  description = "Suffix to append to the name of the resources."
+  type        = string
 }
