29d833af61a52e3e4618edb004a6b8579aafbae2
Taha Junaid <taha@tinyfish.io>
2024-11-11 17:40:45 -0800

    fix incorrect var name usage

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role" "github" {
   name               = var.role_name == null ? "ih-tf-${var.repo_name}-github" : var.role_name
-  description        = "Role for a GitHub Actions runner in repo ${var.repo_name} with suffix ${var.name_suffix}"
+  description        = "Role for a GitHub Actions runner in repo ${var.repo_name}"
   assume_role_policy = data.aws_iam_policy_document.github-trust.json
   tags               = local.tags
 }
